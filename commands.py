4468fb2b43e23b8a610dcf4c7dd17b76b5307e2c
Biowulf513 <cherepanov92@gmail.com>
2018-04-16 23:16:26 +0500

    Тестовые методы

--- commands.py
+++ commands.py
@@ -1,10 +1,34 @@
 # -*- coding: utf-8 -*-
 
 class NewPost:
-    test_message = 'it"s NewPost'
+
+    def perform(self):
+        print('new post')
+
 
 class UserPosts:
-    test_message = 'it"s UserPosts'
+
+    def search_message(self, username):
+        user_db = {'test': ['message 1','message 2','message 3','message 4','message 5','message 6']}
+        return user_db[username]
+    
+    def perform(self):
+        for message in self.search_message('test'):
+            print(message)
+
 
 class History:
-    test_message = 'it"s History'
+    def read_db(self):
+        db = {'1': 'NewPost',
+              '2': 'UserPosts',
+              '3': 'UserPosts',
+              '4': 'NewPost',
+              '5': 'NewPost',
+              '6': 'UserPosts',
+              '7': 'NewPost'}
+        
+        return db
+
+    def perform(self):
+        for read in self.read_db():
+            print(self.read_db()[read])
