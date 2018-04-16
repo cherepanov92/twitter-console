47396ca3dd6e7fdb9d7e7f1f1a5f3e85dd0455af
Biowulf513 <cherepanov92@gmail.com>
2018-04-16 22:13:48 +0500

    Подключение модуля комманд

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 # -*- coding: utf-8 -*-
+from commands import NewPost, UserPosts, History
+
+def get_routes():
+
+    return{
+        'New post':NewPost,
+        'User posts':UserPosts,
+        'History':History
+    }
+
+
+def perform_command(command):
+    routes = get_routes()
+
+    command_class = routes[command]
+    print(command_class.test_message)
 
 def parse_user_input():
     input_function = input
@@ -10,6 +26,7 @@ def parse_user_input():
 def main():
     while True:
         command = parse_user_input()
+        perform_command(command)
 
 if __name__ == '__main__':
     main()
