7c00bf4af6f7395a1415f83c31607ee5f911a74d
Biowulf513 <cherepanov92@gmail.com>
2018-04-16 22:13:04 +0500

    Создане модуля комманд (тестовые затычки)

---
+++ commands.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+class NewPost:
+    test_message = 'it"s NewPost'
+
+class UserPosts:
+    test_message = 'it"s UserPosts'
+
+class History:
+    test_message = 'it"s History'
