6f52fd419ea18638cc06381e207d83bd16ac9fe6
Biowulf513 <cherepanov92@gmail.com>
2018-04-17 22:37:17 +0500

    проверяем существует ли таблица если нет создаём чистую

--- db_agent.py
+++ db_agent.py
@@ -2,26 +2,33 @@
 import sqlite3
 from datetime import datetime
 
-
 class DataBase:
     conn = sqlite3.connect("twitter.sqlite")
     cursor = conn.cursor()
+    def __init__(self):
+        self.check_db()
+
+    def check_db(self):
+        sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='request';"
+        self.cursor.execute(sql)
+        if not self.cursor.fetchall():
+            print('*** create db ***')
+            self.create_table()
+
+
 
     def create_table(self):
         self.cursor.execute("""CREATE TABLE request (
                                 id INTEGER PRIMARY KEY,
                                 command varchar(100) NOT NULL, 
                                 date varchar(40) NOT NULL)""")
-        self.conn.close()
 
     def record_info(self, command):
         now = datetime.now()
         self.cursor.execute("INSERT INTO request VALUES (NULL , '{command}','{time}')".format(command=command, time=now.strftime("%d-%m-%Y %H:%M")))
         self.conn.commit()
-        self.conn.close()
 
     def cleaner(self):
         sql = "DELETE FROM request"
         self.cursor.execute(sql)
         self.conn.commit()
-        self.conn.close()
